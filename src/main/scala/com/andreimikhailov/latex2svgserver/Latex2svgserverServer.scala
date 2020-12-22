c2f50c8b8f180da53afc22d50aaba3e271ce7f35
Andrei Mikhailov <amkhlv@gmail.com>
2020-12-22 20:21:45 -0300

    cleanup

--- src/main/scala/com/andreimikhailov/latex2svgserver/Latex2svgserverServer.scala
+++ src/main/scala/com/andreimikhailov/latex2svgserver/Latex2svgserverServer.scala
@@ -3,7 +3,6 @@ package com.andreimikhailov.latex2svgserver
 import cats.effect.{ConcurrentEffect, Timer}
 import cats.implicits._
 import fs2.Stream
-import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
@@ -43,29 +42,16 @@ object Latex2svgserverServer {
   scala.xml.XML.save(bystroServerConfFile, bystroConf, "UTF-8", true)
 
   def stream[F[_]: ConcurrentEffect](implicit T: Timer[F]): Stream[F, Nothing] = {
-    for {
-      client <- BlazeClientBuilder[F](global).stream
-      helloWorldAlg = HelloWorld.impl[F]
-      jokeAlg = Jokes.impl[F](client)
-
-      // Combine Service Routes into an HttpApp.
-      // Can also be done via a Router if you
-      // want to extract a segments not checked
-      // in the underlying routes.
-      httpApp = (
-        Latex2svgserverRoutes.helloWorldRoutes[F](helloWorldAlg) <+>
-        Latex2svgserverRoutes.jokeRoutes[F](jokeAlg) <+>
+    val  httpApp = (
         Latex2svgserverRoutes.bibtexRoutes[F](tokenRandom, btParser.parse(new FileReader(bibFile))) <+>
         Latex2svgserverRoutes.formulaRoutes[F](tokenRandom)
       ).orNotFound
 
       // With Middlewares in place
-      finalHttpApp = Logger.httpApp(true, true)(httpApp)
-
-      exitCode <- BlazeServerBuilder[F](global)
+    val  finalHttpApp = Logger.httpApp(true, true)(httpApp)
+    BlazeServerBuilder[F](global)
         .bindHttp(port, ip)
         .withHttpApp(finalHttpApp)
         .serve
-    } yield exitCode
   }.drain
 }
