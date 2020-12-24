15a86e253bc9d3c5594d70a2e877ce42aa3b32e1
Andrei Mikhailov <amkhlv@gmail.com>
2020-12-24 12:16:14 -0300

    some tests

--- src/test/scala/com/andreimikhailov/latex2svgserver/LaTeX2SVGSpec.scala
+++ src/test/scala/com/andreimikhailov/latex2svgserver/LaTeX2SVGSpec.scala
@@ -15,23 +15,38 @@ class LaTeX2SVGSpec extends org.specs2.mutable.Specification {
     "return SVG" >> {
       uriReturnsSVG()
     }
+    "prevent CSRF" >> {
+      uriReturns403OnBadToken()
+    }
+    "prevent foreign origin" >> {
+      uriReturns403OnBadHeader()
+    }
   }
   
-  private[this] val retLaTeX2SVG: Response[IO] = {
+  private[this] def retLaTeX2SVG(goodHeader: Boolean, goodToken: Boolean): Response[IO] = {
 
     val getSVG = Request[IO](
       Method.POST, 
       uri"/svg",
       HttpVersion.`HTTP/2.0`,
-      Headers.of(Header("BystroTeX", "yes"))
-    ).withEntity(FormulaForm("TOKENTOKEN","x",20,"255:255:255","0:0:0"))
+      if (goodHeader) Headers.of(Header("BystroTeX", "yes")) else Headers.of()
+    ).withEntity(FormulaForm(if (goodToken) "TOKENTOKEN" else "","x",20,"255:255:255","0:0:0"))
 
     Latex2svgserverRoutes.formulaRoutes[IO]("TOKENTOKEN").orNotFound(getSVG).unsafeRunSync()
   }
 
   private[this] def uriReturns200(): MatchResult[Status] =
-    retLaTeX2SVG.status must beEqualTo(Status.Ok)
+    retLaTeX2SVG(true, true).status must beEqualTo(Status.Ok)
 
-  private[this] def uriReturnsSVG(): MatchResult[String] =
-    retLaTeX2SVG.as[String].unsafeRunSync() must beEqualTo("{\"message\":\"Hello, world\"}")
+  private[this] def uriReturnsSVG(): MatchResult[String] = {
+    val svg = scala.xml.XML.loadString(retLaTeX2SVG(true,true).as[String].unsafeRunSync())
+    svg.head.label must beEqualTo("svg")
+  }
+
+  private[this] def uriReturns403OnBadToken(): MatchResult[Status] =
+    retLaTeX2SVG(true, false).status must beEqualTo(Status.Forbidden)
+
+  private[this] def uriReturns403OnBadHeader(): MatchResult[Status] =
+    retLaTeX2SVG(false, true).status must beEqualTo(Status.Forbidden)
+
 }
